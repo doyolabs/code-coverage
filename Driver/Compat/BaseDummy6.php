c4d244c0f26cb98a06a2c91f0d29bb58c90200da
Anthonius Munthi <me@itstoni.com>
2019-06-06 20:13:34 +0800

    refactored CodeCoverage to Processor to prevent confusion with original PHP CodeCoverage

---
+++ Driver/Compat/BaseDummy6.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the doyo/behat-coverage-extension project.
+ *
+ * (c) Anthonius Munthi <me@itstoni.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Doyo\Behat\Coverage\Bridge\CodeCoverage\Driver\Compat;
+
+use SebastianBergmann\CodeCoverage\Driver\Driver;
+
+class BaseDummy6 implements Driver
+{
+    public function start(bool $determineUnusedAndDead = true): void
+    {
+    }
+
+    public function stop(): array
+    {
+        return [];
+    }
+}
