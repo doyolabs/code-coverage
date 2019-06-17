8fb2f608d09702a368803faa6a59475ebdb9c7a2
Anthonius Munthi <me@itstoni.com>
2019-06-17 11:08:25 +0800

    added goutte driver for dev environment

--- src/bridge/Resources/fixtures/vendor/autoload.php
+++ src/bridge/Resources/fixtures/vendor/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
-$autoloadFile = __DIR__.'/../../../vendor/autoload.php';
-if(is_file($test = __DIR__.'/../../../../../vendor/autoload.php')){
+$autoloadFile = __DIR__.'/../../../../../vendor/autoload.php';
+if(is_file($test = __DIR__.'/../../../vendor/autoload.php')){
     $autoloadFile = $test;
 }
 
